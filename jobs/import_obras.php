0b1dceb35cf189c787c3552daf0d9fbfa8733590
Luigi <40548409+LuigiTaka@users.noreply.github.com>
2018-09-04 00:06:26 -0300

    Update import_obras.php

--- jobs/import_obras.php
+++ jobs/import_obras.php
@@ -1,22 +1,22 @@
 <?php 
 require_once(__DIR__.'\..\utils.php');
-
+//Os três primeiros autores ficam com {id_autor: null}, não consegui resolver essa parte :/ e também não rolou fazer tudo dentro de um if só.
 $file = file_get_contents("http://htput.com/luigi_project/obras?contentType=text/plain"); // Pega o conteudo do link
 
 $array = explode("\n", $file); // divide por quebra de linha
-
-for ($i=0; $i < count($array) ; $i++) {   
-	$array[$i] = explode(';', $array[$i]); // divide cada $array por ;
-	if (is_array($array[$i])) {
-		if (!getRegistro('autores','nome',$array[$i][1])) { // se o registro não existir efetua o registro
-			addRegistro('autores',['nome' => $array[$i][1]]);
-			echo "Registro efetuado<br>";
-		}elseif ($a = getRegistro('autores','nome',$array[$i][1])) { //se o registro existir efetua o registro em obras.
-			if (!getRegistro('obras', 'obra', $array[$i][0])) { // se não existir registros no arquivo obras, registra! OBS: da erro na primeira vez por que o arquivo ainda não foi criado :v
-					addRegistro('obras',['id_autor' => $a['id'], 'obra' => $array[$i][0]]); 
-			}
-		}
+for ($i=0; $i < count($array) ; $i++) { 
+	$array[$i] = explode(';', $array[$i]);
+	$b = getRegistro('autores','nome',$array[$i][1]);
+	
+	if (!$a = getRegistro('autores','nome',$array[$i][1])) {
+		$s = addRegistro('autores',['nome' => $array[$i][1]]);
+		$f = addRegistro('obras',['obra' => $array[$i][0],'id_autor' => $b['id']]);
+		
+	}elseif (!getRegistro('obras','id_autor',$array[$i][0])) {
+		getRegistro('obras','obra',$array[$i][0]);
+		addRegistro('obras',['obra'=> $array[$i][0], 'id_autor' => $b['id']]);
 	}
-}
+	
 
+}
 
