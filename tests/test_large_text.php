09cc1c28c63717c8c85ee51959634f7fe2ef0ae1
Fabio <ti@traca.com.br>
2018-09-05 13:06:17 -0300

    Corrige barras invertidas no include

--- tests/test_large_text.php
+++ tests/test_large_text.php
@@ -1,5 +1,5 @@
 <?php 
-include_once  __DIR__."\..\utils.php";
+include_once  __DIR__."/../utils.php";
 
 $conteudo = file_get_contents("http://htput.com/luigi_project/obra-100");
 $id_gerado = addLargeText($conteudo);
