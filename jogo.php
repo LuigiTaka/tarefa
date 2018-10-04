0ee5862b1d51c8b47ca6e481822671550b13b326
Luigi <luigimendeszanchett@gmail.com>
2018-10-03 21:40:46 -0300

    testando ainda

--- jogo.php
+++ jogo.php
@@ -1,42 +1,67 @@
 <?php 
 require_once(__DIR__."/utils.php");
-
+session_start();
 $page = new Page();
-
+$_SESSION['player'] = "Lima Barreto";//getRegistro('autores','id',rand(1,6))['nome'];
 $page->title = "Jogo de letras";
 
 	$colunas = 6;
 	$linhas = 6;
-	$controle = 12;
+	$found = 1;
 	$letras = [];
-	for ($i=0; $i < 100 ; $i++) { 
-		$letras[] = $random_string = chr(rand(65,90));
+	for ($i=0; $i < 36 ; $i++) { 
+		$letras[] = chr(rand(65,90));
 	}
 
-	$empty_at = [rand(1,6),rand(1,6)];
-	
+$empty_at = [rand(1,6),rand(1,6)];
+
+
+if (getRegistro('teste_jogo','nome',$_SESSION['player'])){
+	echo "Logou como ".$_SESSION['player'];
+}
+
+$get = getRegistro('teste_jogo','nome',$_SESSION['player']);
+
+if (!empty($_GET['linha'] && !empty($_GET['coluna']))) {
+    settype($_GET['linha'], "integer");
+    settype($_GET['coluna'], "integer");
+    $get['vazio'] = [$_GET['coluna'],$_GET['linha']];            
+}
+echo "<br>";
+var_dump($get['vazio']);
+
 ob_start();
 
 ?>
+<div>
+	<p>Logado como &#8611; <?php echo $_SESSION['player']; ?></p>
+</div>
+
 <table id="table">
     <?php 
+ 
     	for ($n=1; $n <=$colunas ; $n++) { 
     		echo "<tr>";
 	    		for ($f=1; $f <=$linhas ; $f++) { 
-	    			
-	    			if ($n === $empty_at[0] and $f === $empty_at[1]) {
-	    				echo "<td class='empty'> <a href='?a=0'>".$letras[rand(0,99)]."</a></td>";
-	    			}else{
-	    				echo "<td value = ".($f+$n+6)."><a href='?a=1'>".$letras[rand(0,99)]."</a> </td>";
-	    			}
-	    			
+        			if ($n === $get['vazio'][0] and $f === $get['vazio'][1]) {
+        				echo "<td class='empty'></td>";
+        			}else{
+        				echo "<td><a href='?linha=$f&coluna=$n'>".$get['tabela'][rand(0,35)]."</a>
+
+                        </td>";
+		    		}	
 	    		}
     		echo "</tr>";	
-    	}
+    	}		
+
+
+        
+    
     	
      ?>
 </table>
-    
+
+
     <style type="text/css">
      body{
         background:gainsboro;
