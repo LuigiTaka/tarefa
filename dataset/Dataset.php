eabe712213d83e8978da42e0127d893d0ba3363d
LuigiTaka <luigimendeszanchett@gmail.com>
2018-11-20 00:11:15 -0200

    Filter até a tarefa 2 funcionando

--- dataset/Dataset.php
+++ dataset/Dataset.php
@@ -3,6 +3,8 @@
 class Dataset{
 
     var $items = [];
+    var $filtrado;
+
 
     function __construct(array $items)
     {
@@ -14,4 +16,28 @@ class Dataset{
         return $this->items;
 
     }
+
+
+    function filter(Closure $anonimo=null){
+    	
+    	if (isset($anonimo)) {
+    		$caminha = (array_map($anonimo, $this->items));
+   
+    		for ($i=0; $i < count($this->items) ; $i++) { 
+    			if ($caminha[$i] == 1) {
+    				$this->filtrado[] = $this->items[$i]; 
+    			}
+    		}
+    		
+    		return $this->filtrado;
+    	}
+
+    	return $this->items;
+	
+    }
+
+    
+
 }
+
+
